16202fa59ee08303ca2a08732413545f1a14f130
Niall O'Kane <okanenm@gmail.com>
2015-09-09 15:25:33 -0700

    Modify FishSpider to crawl recursively

--- flyfish_chile/flyfish_chile/spiders/fish_spider.py
+++ flyfish_chile/flyfish_chile/spiders/fish_spider.py
@@ -1,26 +1,25 @@
-from scrapy import Spider
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
 from flyfish_chile.items import FlyfishChileItem
 
-class FishSpider(Spider):
+class FishSpider(CrawlSpider):
     name = "fish"
     allowed_domains = ["moldychum.com"]
     start_urls = [
         "http://www.moldychum.com/home-old/",
     ]
 
-    def parse(self, response):
-        # headers = hxs.selector('//a[contains(@href, "")]/@href').extract()
-        headers = Selector(response).xpath('//a[contains(@class,"journal-entry-navigation-current") and contains(@href,"home-old")]')
-        print "TEST!!!!"
-        print headers
-        for head in headers:
-            print "HEAD"
-            print 'http://www.moldychum.com' + head.xpath('@href').extract()[0]
-            # x = head.xpath('/text()').extract()
+    rules = (Rule (LinkExtractor(allow=("", ),restrict_xpaths=('//span[@class="paginationControlNextPageSuffix"]',))
+    , callback="parse_page", follow= True),)
 
+    print rules
+
+    def parse_page(self, response):
+        headers = Selector(response).xpath('//a[contains(@class,"journal-entry-navigation-current") and contains(@href,"home-old")]')
+        for head in headers:
             item = FlyfishChileItem()
-            # titleNode = head[0]
             item['title'] = head.xpath('text()').extract()[0]
             item['url'] = 'http://www.moldychum.com' + head.xpath('@href').extract()[0]
             yield item
